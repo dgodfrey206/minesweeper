7b54eb717831fb81caac0c8615a8e97136ee9cda
David Godfrey <david.godfrey99@gmail.com>
2022-04-16 19:27:15 -0400

    Grid made

--- main.cpp
+++ main.cpp
@@ -6,31 +6,41 @@ public:
     Simple(const wxString& title) 
         : wxFrame(NULL, wxID_ANY, title, wxPoint(30, 30), wxSize(800, 600))
 	{
-		Centre();
-        m_button = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
-        m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(100, 30));
-        m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+        btns = new wxButton*[nFieldWidth * nFieldHeight];
+        wxGridSizer* grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
+        for (int i = 0; i < nFieldWidth*nFieldHeight; i++) {
+            btns[i] = new wxButton(this, 1000 + i, " ");
+            grid->Add(btns[i], 1, wxEXPAND | wxALL);
+        }
+        this->SetSizer(grid);
+        grid->Layout();
+        Centre();
 	}
+    
 private:
-    wxButton* m_button = nullptr;
-    wxTextCtrl* m_txt1 = nullptr;
-    wxListBox* m_list1 = nullptr;
+    int nFieldHeight = 10;
+    int nFieldWidth = 10;
+    wxButton** btns = nullptr;
+
+
+    //wxDECLARE_EVENT_TABLE();
 };
+
+/*wxBEGIN_EVENT_TABLE(Simple, wxFrame)
+    EVT_BUTTON(10001, Simple::onButtonClicked)
+    EVT_LISTBOX(10003, Simple::onKeyPress)
+    EVT_BUTTON(10004, Simple::deleteClick)
+wxEND_EVENT_TABLE()*/
 
 class MyApp : public wxApp
 {
 public:
-    MyApp() {
-        
-    }
-
 	bool OnInit()
 	{
 		Simple *simple = new Simple(wxT("Minesweeper"));
 		simple->Show(true);
 		return true;
 	}
-private:
 };
 
 wxIMPLEMENT_APP(MyApp);
