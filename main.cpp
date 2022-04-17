f84fda94303ffa5ef6a82c7e727e0c9c6d2ac220
David Godfrey <david.godfrey99@gmail.com>
2022-04-17 00:01:44 -0400

    Right click, style additions

--- main.cpp
+++ main.cpp
@@ -7,16 +7,24 @@ class Minesweeper : public wxFrame {
   Minesweeper(const wxString& title)
       : wxFrame(NULL, wxID_ANY, title, wxPoint(30, 30), wxSize(800, 600)) {
     wxGridSizer* grid = new wxGridSizer(n, n, 0, 0);
+
+    wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-        board[i][j] = new wxButton(this, 10000 + i, " ");
+        board[i][j] = new wxButton(this, 10000 + idx(i, j), " ");
         grid->Add(board[i][j], 1, wxEXPAND | wxALL);
+        board[i][j]->SetLabel("#");
+        board[i][j]->SetFont(font);
+        board[i][j]->SetBackgroundColour(wxColour(49, 11, 154, 50));
         board[i][j]->Bind(wxEVT_COMMAND_BUTTON_CLICKED,
-                          &Minesweeper::on_button_clicked, this);
+                          &Minesweeper::on_left_click, this);
+        board[i][j]->Bind(wxEVT_RIGHT_DOWN, &Minesweeper::on_right_click, this);
       }
     }
 
-    for (int i = 0, nMines = 10; i < nMines;) {
+
+
+    for (int i = 0, nMines = 30; i < nMines;) {
       int y = rand() % n, x = rand() % n;
       if (!isMine(y, x)) {
         mines.push_back(idx(y, x));
@@ -48,24 +56,38 @@ class Minesweeper : public wxFrame {
   wxButton* board[n][n];
   std::vector<int> mines;
 
-  void on_button_clicked(wxCommandEvent& evt) {
+  void on_left_click(wxCommandEvent& evt) {
     int y = (evt.GetId() - 10000) / n;
     int x = (evt.GetId() - 10000) % n;
-
-    gameOver = isMine(y, x);
-
-    if (gameOver) {
+    
+    if (gameOver || isMine(y, x)) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (isMine(i, j)) {
+                    board[i][j]->SetBackgroundColour(wxColour(207, 21, 33, 70));
                     board[i][j]->SetLabel("*");
                 }
             }
         }
+        wxMessageBox("GAMEOVER");
+        gameOver = true;
         return;
     }
+    board[y][x]->Enable(false);
+    
     board[y][x]->SetLabel(dfs(y, x));
     evt.Skip();
+  }
+
+  void on_right_click(wxMouseEvent& evt) {
+    int y = (evt.GetId() - 10000) / n;
+    int x = (evt.GetId() - 10000) % n;
+    if (board[y][x]->GetLabel() == "?")
+        board[y][x]->SetLabel("M");
+    else if (board[y][x]->GetLabel() == "M")
+        board[y][x]->SetLabel("#");
+    else if (board[y][x]->GetLabel() == "#")
+        board[y][x]->SetLabel("?");
   }
 
   std::string dfs(bool(&visited)[n][n], int y, int x) {
@@ -81,12 +103,14 @@ class Minesweeper : public wxFrame {
       else
         neighbors.push_back({y + dy[i], x + dx[i]});
     }
+    
     if (count == 0) {
       for (auto p : neighbors) {
         board[p.first][p.second]->SetLabel(
             dfs(visited, p.first, p.second));
       }
-      return "#";
+      board[y][x]->SetBackgroundColour(wxColour(*wxWHITE));
+      return " ";
     }
     return std::to_string(count);
   }
@@ -96,14 +120,19 @@ class Minesweeper : public wxFrame {
     return dfs(visited, y, x);
   }
 
-  // wxDECLARE_EVENT_TABLE();
+  void keyPressed(wxMouseEvent& event) {
+      cout<<565<<endl;
+    //   if (event.GetKeyCode() == WXK_ESCAPE) {
+    //       std::exit(0);
+    //   }
+  }
+
+  wxDECLARE_EVENT_TABLE();
 };
 
-/*wxBEGIN_EVENT_TABLE(Simple, wxFrame)
-    EVT_BUTTON(10001, Simple::onButtonClicked)
-    EVT_LISTBOX(10003, Simple::onKeyPress)
-    EVT_BUTTON(10004, Simple::deleteClick)
-wxEND_EVENT_TABLE()*/
+ wxBEGIN_EVENT_TABLE(Minesweeper, wxFrame)
+    EVT_MOTION(Minesweeper::keyPressed)
+ wxEND_EVENT_TABLE()
 
 class MyApp : public wxApp {
  public:
