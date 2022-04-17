b306362362ca2a3463a33a34c67422ded055d272
David Godfrey <david.godfrey99@gmail.com>
2022-04-17 14:17:43 -0400

    Update event handler

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@ class Minesweeper : public wxFrame {
  public:
   Minesweeper(const wxString& title)
       : wxFrame(NULL, wxID_ANY, title, wxPoint(30, 30), wxSize(800, 600)) {
-    wxPanel* mainPane = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxWANTS_CHARS);
-    mainPane->Bind(wxEVT_CHAR_HOOK, &Minesweeper::onKeyDown, this);
-
+    // Create an nxn grid matching the screen proportions
     wxGridSizer* grid = new wxGridSizer(n, n, 0, 0);
 
     wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD,
@@ -25,6 +23,8 @@ class Minesweeper : public wxFrame {
       }
     }
 
+    // 30 mines placed in random positions
+    std::srand(std::time(0));
     for (int i = 0, nMines = 30; i < nMines;) {
       int y = rand() % n, x = rand() % n;
       if (!isMine(y, x)) {
@@ -36,11 +36,14 @@ class Minesweeper : public wxFrame {
     this->SetSizer(grid);
     grid->Layout();
     Centre();
+    this->connectKeyDownEvent(this);
   }
 
   bool valid_index(int i, int j) { return 0 <= i && i < n && 0 <= j && j < n; }
+  // 2d index -> 1d index
   int idx(int y, int x) const { return y * n + x; }
 
+  // is a mine located in the array of mines
   bool isMine(int y, int x) const {
     for (int m : mines) {
       if (m == idx(y, x))
@@ -51,11 +54,25 @@ class Minesweeper : public wxFrame {
 
  private:
   static constexpr int n = 10;
+  // index for each cell neighbor
   int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
   int dy[8] = {1, 1, 1, 0, 0, -1, -1, -1};
   bool gameOver = false;
   wxButton* board[n][n];
   std::vector<int> mines;
+  
+  void connectKeyDownEvent(wxWindow* pclComponent) {
+    if (pclComponent) {
+      pclComponent->Bind(wxEVT_KEY_DOWN, &Minesweeper::onKeyDown, this);
+
+      wxWindowListNode* pclNode = pclComponent->GetChildren().GetFirst();
+      while (pclNode) {
+        wxWindow* pclChild = pclNode->GetData();
+        this->connectKeyDownEvent(pclChild);
+        pclNode = pclNode->GetNext();
+      }
+    }
+  }
 
   void on_left_click(wxCommandEvent& evt) {
     int y = (evt.GetId() - 10000) / n;
@@ -75,8 +92,13 @@ class Minesweeper : public wxFrame {
       return;
     }
 
+    // disable button
     board[y][x]->Enable(false);
+    // clears all nearby cells which have 0 mines as neighbors or sets value at
+    // the cell to the
+    // number of mine neighbors
     board[y][x]->SetLabel(dfs(y, x));
+    // do not propogate event
     evt.Skip();
   }
 
@@ -99,16 +121,19 @@ class Minesweeper : public wxFrame {
     for (int i = 0; i < 8; i++) {
       if (!valid_index(y + dy[i], x + dx[i]) || visited[y + dy[i]][x + dx[i]])
         continue;
+      // count all neighbors that are not mines
       if (isMine(y + dy[i], x + dx[i]))
         count++;
       else
         neighbors.push_back({y + dy[i], x + dx[i]});
     }
 
+    // if there are zero neighbors that are mines, visit each neighbor
     if (count == 0) {
       for (auto p : neighbors) {
         board[p.first][p.second]->SetLabel(dfs(visited, p.first, p.second));
       }
+      // blank cell
       board[y][x]->SetBackgroundColour(wxColour(*wxWHITE));
       return " ";
     }
@@ -121,13 +146,12 @@ class Minesweeper : public wxFrame {
   }
 
   void onKeyDown(wxKeyEvent& event) {
+      std::cout<<"keydown\n";
     if (event.GetKeyCode() == WXK_ESCAPE) {
       std::exit(0);
     }
-    event.Skip();
   }
 };
-
 
 class MyApp : public wxApp {
  public:
